f2c5c4fad8dc16af24e101b2af834c9dc3cd458f
yud <chanduytran51@gmail.com>
2024-05-16 19:25:07 +0700

    Popular

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import NewCollections from "../Components/NewCollections/NewCollections";
 import Footer from "../Components/Footer/Footer";
+import Popular from "../Components/Popular/Popular";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <NewCollections />
+      <Popular/>
       <Footer/>
     </div>
   );

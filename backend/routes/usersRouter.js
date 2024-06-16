9aa9f23c42822909987f712547c182f048efda90
WeeHaw1812 <phuochung1812@gmail.com>
2024-06-16 15:11:42 +0700

    create api for cart & login

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -55,4 +55,81 @@ usersRouter.post("/login", async (req, res) => {
     res.json({ success: false, error: "Wrong Email" });
   }
 });
+// Middle ware to fetchUser
+const fetchUser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ error: "Please authenticate using valid token" });
+  } else {
+    try {
+      const data = jwt.verify(token, "secret_ecom");
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res.status(401).send({ error: "Please authenticate using a valid token" });
+    }
+  }
+};
+// Add To Cart
+usersRouter.post("/addtocart", fetchUser, async (req, res) => {
+  console.log("Added", req.body.itemId);
+  let userData = await Users.findOne({
+    _id: req.user.id,
+  });
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate(
+    {
+      _id: req.user.id,
+    },
+    {
+      cartData: userData.cartData,
+    }
+  );
+  res.send("Added");
+});
+
+// Reduce From Cart
+usersRouter.post("/reducefromcart", fetchUser, async (req, res) => {
+  console.log("reduced", req.body.itemId);
+  let userData = await Users.findOne({
+    _id: req.user.id,
+  });
+  if (userData.cartData[req.body.itemId] > 0) {
+    userData.cartData[req.body.itemId] -= 1;
+  }
+  await Users.findOneAndUpdate(
+    {
+      _id: req.user.id,
+    },
+    {
+      cartData: userData.cartData,
+    }
+  );
+  res.send("Removed");
+});
+// Remove From Cart
+usersRouter.post("/removefromcart", fetchUser, async (req, res) => {
+  console.log("removed", req.body.itemId);
+  let userData = await Users.findOne({
+    _id: req.user.id,
+  });
+  if (userData.cartData[req.body.itemId] > 0) {
+    userData.cartData[req.body.itemId] = 0;
+  }
+  await Users.findOneAndUpdate(
+    {
+      _id: req.user.id,
+    },
+    {
+      cartData: userData.cartData,
+    }
+  );
+  res.send("Removed");
+});
+// Get User Cart Data
+usersRouter.post("/getallcart", fetchUser, async (req, res) => {
+  console.log("Get User Cart Data");
+  let userCartData = await Users.findOne({ _id: req.user.id });
+  res.json(userCartData.cartData);
+});
 module.exports = usersRouter;
